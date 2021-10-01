69ba3144e3470f4f4cdfa4fbd926873284002953
vivek7009 <91315724+vivek7009@users.noreply.github.com>
2021-10-01 12:35:26 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,17 @@
   <div id="app">
     <div class="container">
       <div class="card-columns">
-
-        <div class="card p-3"
-          v-for="post in posts" :key="post.id">
-
+        
+        <div class="card p-3" >
+            <p> {{ title }}</p>
+ 
          </div>
 
       </div>
 
-      <div class="text-center"
-        v-show="postsLoading">
-         Loading...
-      </div>
+    
     </div>
+  
   </div>
 </template>
 
@@ -26,37 +24,33 @@ const snoowrap = require('snoowrap');
 const r = new snoowrap ({
   userAgent: 'Vivek-Shinde00',
   clientId: 'y0BuNnmycbQe-DLy3arWug',
-  clientSecret: '	OHS7racZqCoShfAwACDGoEvREVX3kg',
+  clientSecret: 'OHS7racZqCoShfAwACDGoEvREVX3kg',
   refreshToken: '1139991457823-xCSDimBNAOSyhpmZb_OKivQXvj7ISA',
-  
-  accessToken:'1139991457823-yFlBpY2OVCBsS_hZroHXHia9xUj05w',
- 
+   accessToken:'1139991457823-yFlBpY2OVCBsS_hZroHXHia9xUj05w',
 });
-r.getHot().map(post => post.title).then(console.log);
+
+r.getHot().map(post =>post.title).then(console.log);
+
 
   export default {
       created () {
         this.getPosts()
       },
-      data () {
-        return {
-          posts: [],
-          postsLoading: false,
-          
+     
+    data ()
+     { return  {
+          title:'Reddit title'
         }
       },
-      methods: {
-          getPosts () {
-           
-
-            
-            console.log(snoowrap);
-            r.getHot().map(post => post.title).then(console.log);
-           
-
-          }
-      }
+   methods:
+   {   
+    getPosts: function ()
+     {
+      return this.message
+     }
   }
+      }
+     
 </script>
 <style>
 </style>
